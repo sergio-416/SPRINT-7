3b3899c76f091d448b21eee3be9210d2b41b988d
Sergio <cmu_416@pm.me>
2026-01-07 20:29:01 +0100

    feat: implement getMovies method (GREEN)

--- src/app/services/movies.ts
+++ src/app/services/movies.ts
@@ -1,8 +1,14 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { TmdbMovieResponse } from '../interfaces/movie';
 
 @Injectable({
   providedIn: 'root',
 })
 export class Movies {
-
+  private readonly http = inject(HttpClient);
+  getMovies(): Observable<TmdbMovieResponse> {
+    return this.http.get<TmdbMovieResponse>('https://api.themoviedb.org/3/discover/movie');
+  }
 }
