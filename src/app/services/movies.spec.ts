2a83bb4cf904511deefce73e0ddf81c4e41ac9d7
Sergio <cmu_416@pm.me>
2026-01-07 19:32:23 +0100

    test: add getMovies Observable return type check (RED)

--- src/app/services/movies.spec.ts
+++ src/app/services/movies.spec.ts
@@ -1,4 +1,6 @@
 import { TestBed } from '@angular/core/testing';
+import { Observable } from 'rxjs';
+import { TmdbMovieResponse } from '../interfaces/movie';
 
 import { Movies } from './movies';
 
@@ -12,5 +14,12 @@ describe('Movies', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should have getMovies method that returns Observable<TmdbMovieResponse>', () => {
+    expect(service.getMovies).toBeDefined();
+    expect(typeof service.getMovies).toBe('function');
+    const result = service.getMovies();
+    expect(result).toBeInstanceOf(Observable);
   });
 });
